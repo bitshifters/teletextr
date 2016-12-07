ad07dc8d50ee0a36058e787e25c4ec617903c296
kieranhj <kieran@kconnell.co.uk>
2016-12-07 08:02:10 +0000

    16-bit precision
    All positions & velocity calculations now in 16-bits.

--- src/fx/particles.asm
+++ src/fx/particles.asm
@@ -1,7 +1,5 @@
 
-; extremely dodgy raster bars
-; copied from Kieran's mode7-sprites cracktro wip
-; uses way too much memory for the quality of the effect!
+; extremely dodgy particle fx
 
 MODE7_particles_addr = &7800
 
@@ -16,18 +14,41 @@ PARTICLES_max = 256
 	.loop
 	STA fx_particles_state, X
 	STA fx_particles_xpos, X
+	STA fx_particles_xposh, X
 	STA fx_particles_ypos, X
+	STA fx_particles_yposh, X
 	STA fx_particles_xvel, X
+	STA fx_particles_xvelh, X
 	STA fx_particles_yvel, X
+	STA fx_particles_yvelh, X
 
 	INX
 	CPX #LO(PARTICLES_max)
 	BNE loop
 
 	\\ Gravity
-	LDA #2
+	STA fx_particles_xacc
+	STA fx_particles_xacc+1
+	STA fx_particles_yacc+1
+
+	LDA #&1f
 	STA fx_particles_yacc
 
+	.return
+	RTS
+}
+
+.fx_particles_get_next_free
+{
+	CLC
+	LDX #0
+	.loop
+	LDA fx_particles_state, X
+	BEQ return
+	INX
+	CPX #LO(PARTICLES_max)
+	BNE loop
+	SEC
 	.return
 	RTS
 }
@@ -44,9 +65,9 @@ PARTICLES_max = 256
 	LDA #1
 	STA fx_particles_state, X
 
-	LDA #40*2
+	LDA #40
 	STA fx_particles_xpos, X
-	LDA #25*2
+	LDA #25
 	STA fx_particles_ypos, X
 
 	TXA
@@ -87,15 +108,51 @@ EQUB 0
 	LDA #1
 	STA fx_particles_state,X
 
-	LDA #40*2
+	\\ Set X&Y pos
+	LDA #0
 	STA fx_particles_xpos, X
-	LDA #25*2
 	STA fx_particles_ypos, X
 
-	LDA fx_particles_table,Y
+	LDA #40
+	STA fx_particles_xposh, X
+	LDA #25
+	STA fx_particles_yposh, X
+
+	\\ Sign extend X&Y vel
+	{
+		LDA fx_particles_table,Y
+		ORA #&7F
+		BMI neg
+		LDA #0
+		.neg
+		STA fx_particles_xvelh, X
+	}
+
+	{
+		LDA fx_particles_table_cos,Y
+		ORA #&7F
+		BMI neg
+		LDA #0
+		.neg
+		STA fx_particles_yvelh, X
+	}
+
+	\\ X vel * 4
+	CLC
+	LDA fx_particles_table, Y
+	ASL A
+	ROL fx_particles_xvelh, X
+	ASL A
+	ROL fx_particles_xvelh, X
 	STA fx_particles_xvel, X
 
-	LDA fx_particles_table_cos,Y
+	\\ Y vel * 4
+	CLC
+	LDA fx_particles_table_cos, Y
+	ASL A
+	ROL fx_particles_yvelh, X
+	ASL A
+	ROL fx_particles_yvelh, X
 	STA fx_particles_yvel, X
 
 	JMP return
@@ -127,6 +184,11 @@ EQUB 0
 ;	.not_s
 
 	JSR fx_particles_spin
+	JSR fx_particles_spin
+	JSR fx_particles_spin
+	JSR fx_particles_spin
+	JSR fx_particles_spin
+	JSR fx_particles_spin
 
 	JSR fx_particles_tick
 	JSR fx_particles_draw
@@ -137,8 +199,10 @@ EQUB 0
 
 .fx_particles_xacc
 EQUB 0
+EQUB 0
 
 .fx_particles_yacc
+EQUB 0
 EQUB 0
 
 .fx_particles_tick
@@ -149,30 +213,50 @@ EQUB 0
 	LDA fx_particles_state, X
 	BEQ next
 
+	\\ Update velocities
+
 	CLC
 	LDA fx_particles_xvel, X
 	ADC fx_particles_xacc
 	STA fx_particles_xvel, X
+	LDA fx_particles_xvelh, X
+	ADC fx_particles_xacc+1
+	STA fx_particles_xvelh, X
 
 	CLC
 	LDA fx_particles_yvel, X
 	ADC fx_particles_yacc
 	STA fx_particles_yvel, X
+	LDA fx_particles_yvelh, X
+	ADC fx_particles_yacc+1
+	STA fx_particles_yvelh, X
+
+	\\ Update positions
 
 	CLC
 	LDA fx_particles_xpos, X
 	ADC fx_particles_xvel, X
 	STA fx_particles_xpos, X
+	LDA fx_particles_xposh, X
+	ADC fx_particles_xvelh, X
+	STA fx_particles_xposh, X
+
+	CMP #PLOT_PIXEL_RANGE_X
+	BCS remove_particle
 
 	CLC
 	LDA fx_particles_ypos, X
 	ADC fx_particles_yvel, X
 	STA fx_particles_ypos, X
+	LDA fx_particles_yposh, X
+	ADC fx_particles_yvelh, X
+	STA fx_particles_yposh, X
+
+	CMP #PLOT_PIXEL_RANGE_Y
+	BCC next
 	
 	\\ Need to determine state update conditions
-	CMP #75*2
-	BCC next
-
+	.remove_particle
 	LDA #0
 	STA fx_particles_state, X
 
@@ -194,17 +278,21 @@ EQUB 0
 
 	.loop
 	STX fx_particles_draw_idx
+	LDA fx_particles_state, X
+	BEQ next
 
-	LDA fx_particles_ypos, X
-	LSR A:TAY
-	LDA fx_particles_xpos, X
-	LSR A:TAX
+	LDA fx_particles_yposh, X
+	TAY
+
+	LDA fx_particles_xposh, X
+	TAX
 
 	{
-		PLOT_PIXEL_CLIPPED
+		PLOT_PIXEL	; CLIPPED?
 	}
 
 	LDX fx_particles_draw_idx
+	.next
 	INX
 	CPX #LO(PARTICLES_max)
 	BNE loop
@@ -215,8 +303,7 @@ EQUB 0
 
 
 
-\\ How much precision do we need?
-\\ Run everything *2 to give small bit of extra precision?
+\\ Let's try everything with 16-bit precision
 
 .fx_particles_state
 SKIP PARTICLES_max
@@ -224,18 +311,31 @@ SKIP PARTICLES_max
 .fx_particles_xpos
 SKIP PARTICLES_max
 
+.fx_particles_xposh
+SKIP PARTICLES_max
+
 .fx_particles_ypos
+SKIP PARTICLES_max
+
+.fx_particles_yposh
 SKIP PARTICLES_max
 
 .fx_particles_xvel
 SKIP PARTICLES_max
 
+.fx_particles_xvelh
+SKIP PARTICLES_max
+
 .fx_particles_yvel
 SKIP PARTICLES_max
 
+.fx_particles_yvelh
+SKIP PARTICLES_max
+
+\\ Apart from sin/cos table - could be reused from another fx?
 .fx_particles_table
 FOR n,0,&13F,1
-EQUB 8 * SIN(2 * PI * n / 255)
+EQUB 127 * SIN(2 * PI * n / 255)
 NEXT
 
 fx_particles_table_cos = fx_particles_table + 64
