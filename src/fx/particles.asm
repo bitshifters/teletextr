608b0d70d361dc715cc7461c9976bd038550bf7b
kieranhj <kieran@kconnell.co.uk>
2016-12-08 18:23:34 +0000

    Added spurt emitter
    Ooer.

--- src/fx/particles.asm
+++ src/fx/particles.asm
@@ -5,14 +5,24 @@ MODE7_particles_addr = &7800
 
 PARTICLES_max = 128
 _PARTICLES_ENABLE_BANG = TRUE
-_PARTICLES_ENABLE_SPIN = TRUE
+_PARTICLES_ENABLE_SPIN = FALSE
+_PARTICLES_ENABLE_SPIN4 = FALSE
+_PARTICLES_ENABLE_SPURT = TRUE
 _PARTICLES_ENABLE_COLOUR = TRUE
+
+PARTICLES_gravity = &0F				; fractional part
 
 PARTICLES_BANG_num = 8
 PARTICLES_BANG_ypos = 15
 
 PARTICLES_SPIN_xpos = 40
 PARTICLES_SPIN_ypos = 25
+
+PARTICLES_SPURT_xpos = 40
+PARTICLES_SPURT_ypos = 60
+PARTICLES_SPURT_centre = 128
+PARTICLES_SPURT_width = 20
+PARTICLES_SPURT_speed = 3
 
 .fx_particles_init
 {
@@ -39,7 +49,7 @@ PARTICLES_SPIN_ypos = 25
 	STA fx_particles_xacc+1
 	STA fx_particles_yacc+1
 
-	LDA #&0f
+	LDA #PARTICLES_gravity
 	STA fx_particles_yacc
 
 	.return
@@ -48,13 +58,11 @@ PARTICLES_SPIN_ypos = 25
 
 .fx_particles_get_next_free_X
 {
-;	STX loop_test+1
 	CLC
 	.loop
 	LDA fx_particles_state, X
 	BEQ return
 	INX
-;	AND #LO(PARTICLES_max-1)
 	.loop_test
 	CPX #LO(PARTICLES_max)
 	BCC loop
@@ -63,6 +71,7 @@ PARTICLES_SPIN_ypos = 25
 	RTS
 }
 
+IF _PARTICLES_ENABLE_BANG
 .fx_particles_bang_xpos
 EQUB 20
 
@@ -94,7 +103,7 @@ EQUB 0
 	LDA #PARTICLES_BANG_ypos
 	STA fx_particles_yposh, X
 
-	JSR fx_particles_set_vel_Y
+	JSR fx_particles_set_vel2_Y
 
 	TYA
 	CLC
@@ -113,8 +122,47 @@ EQUB 0
 
 	RTS
 }
+ENDIF
 
-.fx_particles_set_vel_Y
+.fx_particles_set_vel2_Y
+{
+	\\ Sign extend X&Y vel
+	{
+		LDA fx_particles_table, Y
+		ORA #&7F
+		BMI neg
+		LDA #0
+		.neg
+		STA fx_particles_xvelh, X
+	}
+
+	{
+		LDA fx_particles_table_cos, Y
+		ORA #&7F
+		BMI neg
+		LDA #0
+		.neg
+		STA fx_particles_yvelh, X
+	}
+
+	\\ X vel * 2
+	CLC
+	LDA fx_particles_table, Y
+	ASL A
+	ROL fx_particles_xvelh, X
+	STA fx_particles_xvel, X
+
+	\\ Y vel * 2
+	CLC
+	LDA fx_particles_table_cos, Y
+	ASL A
+	ROL fx_particles_yvelh, X
+	STA fx_particles_yvel, X
+
+	RTS
+}
+
+.fx_particles_set_vel4_Y
 {
 	\\ Sign extend X&Y vel
 	{
@@ -138,8 +186,8 @@ EQUB 0
 	\\ X vel * 4
 	CLC
 	LDA fx_particles_table, Y
-;	ASL A
-;	ROL fx_particles_xvelh, X
+	ASL A
+	ROL fx_particles_xvelh, X
 	ASL A
 	ROL fx_particles_xvelh, X
 	STA fx_particles_xvel, X
@@ -147,8 +195,8 @@ EQUB 0
 	\\ Y vel * 4
 	CLC
 	LDA fx_particles_table_cos, Y
-;	ASL A
-;	ROL fx_particles_yvelh, X
+	ASL A
+	ROL fx_particles_yvelh, X
 	ASL A
 	ROL fx_particles_yvelh, X
 	STA fx_particles_yvel, X
@@ -156,6 +204,7 @@ EQUB 0
 	RTS
 }
 
+IF _PARTICLES_ENABLE_SPIN
 .fx_particles_spin_idx
 EQUB 0
 
@@ -181,13 +230,75 @@ EQUB 0
 	LDA #PARTICLES_SPIN_ypos
 	STA fx_particles_yposh, X
 
-	JSR fx_particles_set_vel_Y
+	JSR fx_particles_set_vel2_Y
 
 	INY			; assumes 256 entries in spin table
 
 	.return
 	RTS
 }
+ENDIF
+
+IF _PARTICLES_ENABLE_SPURT
+.fx_particles_spurt_idx
+EQUB PARTICLES_SPURT_centre
+
+.fx_particles_spurt_dir
+EQUB PARTICLES_SPURT_speed
+
+.fx_particles_spurt_Y
+{
+	JSR fx_particles_get_next_free_X
+	BCS return
+
+	\\ Found a free particle
+	LDY fx_particles_spurt_idx
+	TYA
+	AND #&3
+	CLC
+	ADC #145
+	STA fx_particles_state, X
+
+	\\ Set X&Y pos
+	LDA #0
+	STA fx_particles_xpos, X
+	STA fx_particles_ypos, X
+
+	LDA #PARTICLES_SPURT_xpos
+	STA fx_particles_xposh, X
+	LDA #PARTICLES_SPURT_ypos
+	STA fx_particles_yposh, X
+
+	JSR fx_particles_set_vel4_Y
+
+	LDA fx_particles_spurt_dir
+	BMI neg
+
+	CLC
+	ADC fx_particles_spurt_idx
+	CMP #(PARTICLES_SPURT_centre+PARTICLES_SPURT_width)
+	BCC continue
+	LDA #(256-PARTICLES_SPURT_speed)
+	STA fx_particles_spurt_dir
+	LDA #(PARTICLES_SPURT_centre+PARTICLES_SPURT_width)
+	JMP continue
+	
+	.neg
+	CLC
+	ADC fx_particles_spurt_idx
+	CMP #(PARTICLES_SPURT_centre-PARTICLES_SPURT_width)
+	BCS continue
+	LDA #PARTICLES_SPURT_speed
+	STA fx_particles_spurt_dir
+	LDA #(PARTICLES_SPURT_centre-PARTICLES_SPURT_width)
+
+	.continue
+	STA fx_particles_spurt_idx
+
+	.return
+	RTS
+}
+ENDIF
 
 .fx_particles_update
 {
@@ -211,17 +322,26 @@ IF _PARTICLES_ENABLE_SPIN
 	LDX #0
 	LDY fx_particles_spin_idx
 	JSR fx_particles_spin_Y
-;	INY:INY:INY:INY
+	IF _PARTICLES_ENABLE_SPIN4
+	INY:INY:INY:INY
+	ELSE
 	TYA:CLC:ADC #&40:TAY
+	ENDIF
 	STY fx_particles_spin_idx
 
 ; This gives 4x particles generated per frame - a bit much!
-;	TYA:CLC:ADC #&40:TAY
-;	JSR fx_particles_spin_Y
-;	TYA:CLC:ADC #&40:TAY
-;	JSR fx_particles_spin_Y
-;	TYA:CLC:ADC #&40:TAY
-;	JSR fx_particles_spin_Y
+	IF _PARTICLES_ENABLE_SPIN4
+	TYA:CLC:ADC #&40:TAY
+	JSR fx_particles_spin_Y
+	TYA:CLC:ADC #&40:TAY
+	JSR fx_particles_spin_Y
+	TYA:CLC:ADC #&40:TAY
+	JSR fx_particles_spin_Y
+	ENDIF
+ENDIF
+
+IF _PARTICLES_ENABLE_SPURT
+	JSR fx_particles_spurt_Y
 ENDIF
 
 	JSR fx_particles_tick
@@ -440,13 +560,16 @@ NEXT
 
 fx_particles_table_cos = fx_particles_table + 64
 
+\\ Fast x,y coordinates to character address lookup
+IF _PARTICLES_ENABLE_COLOUR
 .fx_particles_pixel_ytable_lo
 	FOR i, 0, PLOT_PIXEL_RANGE_Y-1
-	  y = (i DIV 3) * 40		; +1 due to graphics chr
+	  y = (i DIV 3) * 40		; +1 due to graphics chr - actually want cell before for colour!
 	  EQUB LO(y-i)	; adjust for (zp),y style addressing, where Y will be the y coordinate
 	NEXT
 .fx_particles_pixel_ytable_hi
 	FOR i, 0, PLOT_PIXEL_RANGE_Y-1
-	  y = (i DIV 3) * 40		; +1 due to graphics chr
+	  y = (i DIV 3) * 40		; +1 due to graphics chr - actually want cell before for colour!
 	  EQUB HI(y-i)	; adjust for (zp),y style addressing, where Y will be the y coordinate
 	NEXT
+ENDIF
