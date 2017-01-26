3b61e5ce3e58678e7eaddd29d0df05540f26a889
kieranhj <kieran@kconnell.co.uk>
2017-01-26 20:07:38 +0000

    Report dotscroll size in swram text

--- teletextr.asm
+++ teletextr.asm
@@ -313,7 +313,6 @@ SAVE "Bank3", bank3_start, bank3_end, &8000
 PRINT "------------------------------------------------------------"
 PRINT " fx_code size is", (end_fx_code-start_fx_code), "bytes"
 PRINT "Main RAM effects:"
-PRINT " fx_dotscroller size is", (end_fx_dotscroller-start_fx_dotscroller), "bytes"
 PRINT " fx_teletext size is", (end_fx_teletext-start_fx_teletext), "bytes"
 PRINT " fx_vectortext size is", (end_fx_vectortext-start_fx_vectortext), "bytes"
 
@@ -332,6 +331,7 @@ PRINT " fx_copperbars size is", (end_fx_copperbars-start_fx_copperbars), "bytes"
 PRINT " fx_particles size is", (end_fx_particles-start_fx_particles), "bytes"
 PRINT " fx_starfield size is", (end_fx_starfield-start_fx_starfield), "bytes"
 PRINT " fx_creditscroll size is", (end_fx_creditscroll-start_fx_creditscroll), "bytes"
+PRINT " fx_dotscroller size is", (end_fx_dotscroller-start_fx_dotscroller), "bytes"
 
 
 PRINT "------------------------------------------------------------"
