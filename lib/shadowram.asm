0c5fb78dd337ae007c7b24fd41277bcd7d380a20
SimonM <podvilla@gmail.com>
2017-03-29 00:43:49 +0100

    Fixed typo from AUG

--- lib/shadowram.asm
+++ lib/shadowram.asm
@@ -1,3 +1,28 @@
+; Accessing & controlling the Shadow RAM is achieved using the Access control register (ACCCON) at &FE34 
+; See page 162 of the New Advanced User Guide (http://www.msknight.com/bbc/manuals/new-advanced-user-guide.pdf)
+; Bit 7 - IRR - IRQ control
+; Bit 6 - TST - always 0
+; Bit 5 - IFJ - 1MHz bus/ROM cartridge select 
+; Bit 4 - ITU - TUBE select
+; Bit 3 - Y - 8Kb RAM function select
+; Bit 2 - X - Main memory RAM source select
+; Bit 1 - E - VDU Driver RAM Source select
+; Bit 0 - D - CRTC RAM Source select
+
+
+; D=0 Use main memory for screen, D=1 Use Shadow RAM for screen (contrary to what is specified in AUG)
+; E=0 VDU Driver uses Shadow RAM, E=1 VDU Driver uses main memory
+; X=0 Normal RAM in main memory, X=1 Shadow RAM in main memory
+; Y=0 8K RAM at &C000 to &DFFF, Y=1 VDU Driver code at &C000
+; ITU=0 enable external TUBE, ITU=1 enable internal TUBE
+; IFJ=0 1Mhz bus at &FC00 to &FDFF, IFJ=1 cartridge at &FC00
+; TST=0 normal state (do not change), TST=1 hardware test
+; IRR=0 after IRQ processed, IRR=1 IRQ to CPU
+
+; so double buffer rendering works as follows:
+;   D=0,X=1 - Display from main memory, Draw to shadow memory (&3000-&7FFF)
+;   D=1,X=0 - Display from shadow memory, Draw to main memory (&3000-&7FFF)
+
 
 ; check if host machine is a Master 128
 ; we do this by testing if memory address &C000 is writable
@@ -17,14 +42,22 @@
     rts
 }
 
+; clear bit 4 of ACCCON, so that the 8Kb Buffer at &C000 can be used as spare RAM instead of MOS VDU buffer
+.shadow_enable_hiram
+{
+    lda &fe34
+    and #255-8  ; set Y to 0
+    sta &fe34
+	rts	
+}
 
 
-
-; we set bits 0 and 2, so that display=Main RAM, and shadow ram is selected as main memory
+; we set bits 0 and 2 of ACCCON, so that display=Main RAM, and shadow ram is selected as main memory
 .shadow_init_buffers
 {
     lda &fe34
-    ora #1+4    ; set D and X to 1
+    ora #4    	; set X to 1
+    and #255-1  ; set D to 0
     and #255-8  ; set Y to 0, so that the 8Kb Buffer can be used as RAM
     sta &fe34
     rts
@@ -36,7 +69,7 @@
 .shadow_swap_buffers
 {
     lda &fe34
-    eor #1+4
+    eor #1+4	; invert bits 0 & 2
     sta &fe34
     rts
 }
