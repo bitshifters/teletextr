9893ffa8a3aac5e537b2f1c32b27ece4d5b055bd
SimonM <podvilla@gmail.com>
2017-04-01 01:22:23 +0100

    Boot loader cleanup

--- src/boot.asm
+++ src/boot.asm
@@ -1,32 +1,27 @@
-
-
-
-IF 0
-.bank_file0    EQUS "Bank0", 13
-.bank_file1    EQUS "Bank1", 13
-.bank_file2    EQUS "Bank2", 13
-.bank_file3    EQUS "Bank3", 13
-.myfile EQUS "Bank0  $"
-ENDIF
+\ ******************************************************************
+\ *	Bootstrap loader code
+\ ******************************************************************
 
 
 .os_load_system   EQUS "LOAD System", 13
 .os_load_main     EQUS "LOAD Main", 13
-;.oscli_sbank0   EQUS "LOAD SBank0"
+.os_load_sbank0   EQUS "LOAD SBank0", 13
+.os_load_sbank1   EQUS "LOAD SBank1", 13
+
 
 ; disk loader uses hacky filename format (same as catalogue) 
+; we use disk loader for SWR banks only
 .bank_file0a   EQUS "Bank0  $"
 .bank_file1a   EQUS "Bank1  $"
 .bank_file2a   EQUS "Bank2  $"
 .bank_file3a   EQUS "Bank3  $"
-.bank_file4a   EQUS "SBank0 $"
-.bank_file5a   EQUS "SBank1 $"
-.bank_file6a   EQUS "Main   $"
+
+
 
 .intro_text0 EQUS "Teletextr OS V1.0", 13, 10, 0
 .intro_text1 EQUS "Initializing Teletext system...", 13, 10, 0
 .master_text EQUS "This demo is compatible with BBC Master 128 Only. :(", 13, 10, 0
-.error_text  EQUS "Guru meditation", 13, 10, 0
+
 
 .boot_entry
 {
@@ -63,13 +58,14 @@ ENDIF
 .swr_fail_text EQUS "No SWR banks found.", 13, 10, 0
 .swr_bank_text EQUS "Found %b", LO(swr_ram_banks_count), HI(swr_ram_banks_count), " SWR banks.", 13, 10, 0
 .swr_bank_text2 EQUS " Bank %a", 13, 10, 0
-.loading_bank_text EQUS "Loading bank", 13, 10, 0
-.loading_bank_text2 EQUS "Bank loaded", 13, 10, 0
+.loading_bank_text EQUS "Loading bank... ", 0
+.loading_bank_text2 EQUS "OK", 13, 10, 0
 .test_print_number EQUS "%a", 13,10,0
 
 
     .swr_ok
 
+IF 0
     MPRINT    swr_bank_text
     ldx #0
 .swr_print_loop
@@ -78,8 +74,7 @@ ENDIF
     inx
     cpx swr_ram_banks_count
     bne swr_print_loop
-    
-    MPRINT loading_bank_text
+ENDIF
 
 IF 0
     ; cat info
@@ -108,34 +103,39 @@ IF 0
     jsr disksys_load_file
 ENDIF
 
+    \\ install main code
+    MPRINT loading_bank_text
+    ldx #LO(os_load_main)
+    ldy #HI(os_load_main)
+    jsr oscli    
+    MPRINT loading_bank_text2
 
-
-	\\ load all banks
+	\\ load all SWR banks
 
     ; SWR 0
+    MPRINT loading_bank_text  
     lda #0
     jsr swr_select_slot
-
     lda #&80
     ldx #LO(bank_file0a)
     ldy #HI(bank_file0a)
     jsr disksys_load_file
-    MPRINT loading_bank_text2
+    MPRINT loading_bank_text2   
 
     ; SWR 1
+    MPRINT loading_bank_text
     lda #1
     jsr swr_select_slot
-
     lda #&80
     ldx #LO(bank_file1a)
     ldy #HI(bank_file1a)
     jsr disksys_load_file
-    MPRINT loading_bank_text2
+    MPRINT loading_bank_text2   
 
     ; SWR 2
+    MPRINT loading_bank_text
     lda #2
     jsr swr_select_slot
-
     lda #&80
     ldx #LO(bank_file2a)
     ldy #HI(bank_file2a)
@@ -143,39 +143,36 @@ ENDIF
     MPRINT loading_bank_text2
 
     ; SWR 3
+    MPRINT loading_bank_text
     lda #3
     jsr swr_select_slot
-
     lda #&80
     ldx #LO(bank_file3a)
     ldy #HI(bank_file3a)
     jsr disksys_load_file
     MPRINT loading_bank_text2
-    
+
+    \\ Install main & shadow ram banks
+
     ; shadow bank 0
+    MPRINT loading_bank_text
     lda #SELECT_RAM_MAIN
     jsr shadow_select_ram
-
-    lda #&30
-    ldx #LO(bank_file4a)
-    ldy #HI(bank_file4a)
-    jsr disksys_load_file
+    ldx #LO(os_load_sbank0)
+    ldy #HI(os_load_sbank0)
+    jsr oscli   
     MPRINT loading_bank_text2
 
     ; shadow bank 1
+    MPRINT loading_bank_text
     lda #SELECT_RAM_SHADOW
     jsr shadow_select_ram
-
-    lda #&30
-    ldx #LO(bank_file5a)
-    ldy #HI(bank_file5a)
-    jsr disksys_load_file
-    MPRINT loading_bank_text2
-
-    ; install main code
-    ldx #LO(os_load_main)
-    ldy #HI(os_load_main)
-    jsr oscli    
+    ldx #LO(os_load_sbank1)
+    ldy #HI(os_load_sbank1)
+    jsr oscli   
+    ; reset ram
+    lda #SELECT_RAM_MAIN
+    jsr shadow_select_ram
     MPRINT loading_bank_text2
 
     ; runtime
